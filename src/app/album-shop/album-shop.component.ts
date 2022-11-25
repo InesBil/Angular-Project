10e9ff9b30f4b04835277a4cbbce5603947655a8
Maria Ines Bilbao <mainesbilbao@gmail.com>
2022-11-25 17:35:13 -0300

    Agrego carpeta enviroments

--- src/app/album-shop/album-shop.component.ts
+++ src/app/album-shop/album-shop.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-album-shop',
   templateUrl: './album-shop.component.html',
   styleUrls: ['./album-shop.component.scss']
 })
-export class AlbumShopComponent {
+export class AlbumShopComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void{
+  
+}
 
 }
+
+
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,16 @@
+import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
+import 'zone.js';
+  
+
+import * as environment from './environments/environment';
+  
+if (environment.environment.production) {
+  enableProdMode();
+}
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
